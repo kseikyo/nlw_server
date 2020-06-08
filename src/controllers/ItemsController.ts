kseikyo/nlw_server

commit ecaea436089c22f06fd403053b01f226d694c6e8

Fix environmental variables on image_urls

src/controllers/ItemsController.ts
@@ -11,7 +11,7 @@ class ItemsController {
       return {
         id: item.id,
         title: item.title,
-        image_url: `${process.env.PORT || 'http://192.168.0.104:3333/uploads/'}${item.image}`
+        image_url: `${process.env.PORT + '/uploads/' || 'http://192.168.0.104:3333/uploads/'}${item.image}`
       }
     })
   

src/controllers/PointsController.ts
@@ -61,7 +61,7 @@ class PointsController {
 
     const serializedPoint = {
       ...point,
-      image_url: `${process.env.SERVER_UPLOADS || 'http://192.168.0.104:3333/uploads/'}${point.image}`
+      image_url: `${process.env.PORT + '/uploads/' || 'http://192.168.0.104:3333/uploads/'}${point.image}`
     };
 
     return response.json(serializedPoint);
@@ -108,7 +108,7 @@ class PointsController {
     const serializedPoints = points.map(point => {
       return {
         ...point,
-        image_url: `${process.env.SERVER_UPLOADS || 'http://192.168.0.104:3333/uploads/'}${point.image}`
+        image_url: `${process.env.PORT + '/uploads/' || 'http://192.168.0.104:3333/uploads/'}${point.image}`
       }
     });
 

commit b4fbbcdca4ab10c72866fa27223747d7d6edc400

Added new env variable as PORT indicates the port the server is running
Fixes: ecaea43

src/controllers/ItemsController.ts
@@ -11,7 +11,7 @@ class ItemsController {
       return {
         id: item.id,
         title: item.title,
-        image_url: `${process.env.PORT + '/uploads/' || 'http://192.168.0.104:3333/uploads/'}${item.image}`
+        image_url: `${process.env.BASE_URL + '/uploads/' || 'http://192.168.0.104:3333/uploads/'}${item.image}`
       }
     })
   

src/controllers/PointsController.ts
@@ -61,7 +61,7 @@ class PointsController {
 
     const serializedPoint = {
       ...point,
-      image_url: `${process.env.PORT + '/uploads/' || 'http://192.168.0.104:3333/uploads/'}${point.image}`
+      image_url: `${process.env.BASE_URL + '/uploads/' || 'http://192.168.0.104:3333/uploads/'}${point.image}`
     };
 
     return response.json(serializedPoint);
@@ -108,7 +108,7 @@ class PointsController {
     const serializedPoints = points.map(point => {
       return {
         ...point,
-        image_url: `${process.env.PORT + '/uploads/' || 'http://192.168.0.104:3333/uploads/'}${point.image}`
+        image_url: `${process.env.BASE_URL + '/uploads/' || 'http://192.168.0.104:3333/uploads/'}${point.image}`
       }
     });
 